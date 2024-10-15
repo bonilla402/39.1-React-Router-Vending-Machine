daf77d637201bd677e854fa0bd01cb703511fdee
nwog-jbonilla <jbonilla@neworg.com>
2024-10-15 19:33:43 -0400

    cute images

--- src/CutePuppies.js
+++ src/CutePuppies.js
@@ -1,16 +1,17 @@
-﻿// src/CutePuppies.js
-import React from 'react';
+﻿import React from 'react';
 import { Link } from 'react-router-dom';
 import './CutePuppies.css';
 
 function CutePuppies() {
     return (
         <div className="cute-puppies">
-            <h1>Cute Puppies</h1>
-            <p>Adorable puppies full of love!</p>
-            <Link to="/" className="cute-puppies-back-link">
-                Back to Vending Machine
-            </Link>
+            <div className="text-overlay">
+                <h1>Cute Puppies</h1>
+                <p>Adorable puppies full of love!</p>
+                <Link to="/" className="cute-puppies-back-link">
+                    Back to Vending Machine
+                </Link>
+            </div>
         </div>
     );
 }
--- src/GrumpyCats.js
+++ src/GrumpyCats.js
@@ -5,11 +5,13 @@ import './GrumpyCats.css';
 function GrumpyCats() {
     return (
         <div className="grumpy-cats">
-            <h1>Grumpy Cats</h1>
-            <p>These cats aren't amused, no matter what you do.</p>
-            <Link to="/" className="grumpy-cats-back-link">
-                Back to Vending Machine
-            </Link>
+            <div className="text-overlay">
+                <h1>Grumpy Cats</h1>
+                <p>These cats aren't amused, no matter what you do.</p>
+                <Link to="/" className="grumpy-cats-back-link">
+                    Back to Vending Machine
+                </Link>
+            </div>
         </div>
     );
 }
